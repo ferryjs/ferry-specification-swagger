684fdb4114eacb49e62e4830d96b32777aebc394
Joshua Jabbour <code@joshuajabbour.com>
2015-03-30 16:55:01 -0700

    Use swagger-parser to parse swagger spec.

--- src/Swagger.js
+++ src/Swagger.js
@@ -1,8 +1,32 @@
 'use strict';
 
+import parser from 'swagger-parser';
+
 import {Specification} from 'ferry';
 
 class Swagger extends Specification {
+
+  initialize(callback) {
+
+    try {
+
+      parser.parse(this.filepath, (err, api, metadata) => {
+
+        this.source = api;
+
+        this.process();
+
+        if (typeof callback === 'function') {
+          callback(err);
+        }
+
+      });
+
+    } catch (err) {
+      throw new Error('Specification source is not valid Swagger');
+    }
+
+  }
 
   process() {
     this.basePath = this.source.basePath;
