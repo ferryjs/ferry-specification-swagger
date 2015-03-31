1275a84f85225d8ab13b87ec3ba0dae5052f23fe
Joshua Jabbour <code@joshuajabbour.com>
2015-03-31 12:06:45 -0700

    Validate within the Specification adapter.

--- src/Swagger.js
+++ src/Swagger.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import parser from 'swagger-parser';
+import validator from 'tv4';
 
 import {Specification} from 'ferry';
 
@@ -33,6 +34,33 @@ class Swagger extends Specification {
     this.version = this.source.info.version;
     this.resources = this.source.definitions;
     this.routes = this.source.paths;
+    this.compileValidators();
+  }
+
+  compileValidators() {
+    this.validators = {};
+    for (let path in this.routes) {
+      for (let method in this.routes[path]) {
+        if (this.routes[path][method].parameters && this.routes[path][method].parameters.length) {
+          this.routes[path][method].parameters.forEach((route) => {
+            if (route.in === 'body') {
+              // Key custom validators by operation.
+              this.validators[this.routes[path][method].operationId] = route.schema;
+            }
+          });
+        }
+      }
+    }
+  }
+
+  getResourceSpecForValidator(resourceType, operation) {
+    return this.validators[operation];
+  }
+
+  validateResource(resourceType, resource, operation, callback) {
+    let result = validator.validateMultiple(resource, this.getResourceSpecForValidator(resourceType, operation), true, true);
+    let res = result.valid ? true : result;
+    return (typeof callback === 'function') ? callback(res) : res;
   }
 
 }
